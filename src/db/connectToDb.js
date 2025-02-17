2cdaa38e16c10ac85ea26c6f3470fdab19e2bb69
uiux@okdollar frontend <uiux@okdollar.com>
2025-02-17 16:31:13 +0630

    db change

--- src/db/connectToDb.js
+++ src/db/connectToDb.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose"
 
 export const connectToDb = async(req,res)=>{
     try {
-        await mongoose.connect(process.env.MONGO_URL).then(()=>{
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000,  // 30 seconds
+            socketTimeoutMS: 45000,           // 45 seconds
+          }).then(()=>{
             console.log('db is connected')
         }).catch((error)=>{
             console.log('db is disconnected.',error)
