04b1a80203d036f344858a1c4a9ceca2a8a69806
uiux@okdollar frontend <uiux@okdollar.com>
2025-02-18 17:19:26 +0630

    vercel fix

--- src/db/connectToDb.js
+++ src/db/connectToDb.js
@@ -2,12 +2,7 @@ import mongoose from "mongoose"
 
 export const connectToDb = async(req,res)=>{
     try {
-        await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 30000,  // 30 seconds
-            socketTimeoutMS: 45000,           // 45 seconds
-          }).then(()=>{
+        await mongoose.connect(process.env.MONGO_URL).then(()=>{
             console.log('db is connected')
         }).catch((error)=>{
             console.log('db is disconnected.',error)
